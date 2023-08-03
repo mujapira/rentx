9184ca36f7044d37b4adab82d0721addfc85ed9d
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2023-08-03 19:42:41 -0300

    dependencies

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,8 +7,12 @@ import Logo from "../../assets/logo.svg";
 export function SideBar() {
   return (
     <nav className="fixed h-full bg-primary w-20 flex flex-col">
-      <div className="w-20 h-20 flex content-center justify-center">
-        <Image src={Logo} alt="rentx-logo" width={100} height={100} />
+      <div className="w-20 h-20 flex content-center justify-center bg-secondary">
+        <Image src={Logo} alt="rentx-logo" width={30} height={20} />
+      </div>
+
+      <div className="flex flex-col">
+        <div></div>
       </div>
     </nav>
   );
