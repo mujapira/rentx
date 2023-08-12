45ae8861d8dcafdf54d4a06c09b3605eb48be1a1
Mujapira <malmasmello2@gmail.com>
2023-08-11 23:28:24 -0300

    Skeletons

--- src/app/(client)/carros/filtros/loading.tsx
+++ src/app/(client)/carros/filtros/loading.tsx
@@ -1,9 +1,29 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-    return (
-        <div>
-            <h1>
-                OI PEDRO
-            </h1>
+  return (
+    <div className="h-full min-h-[calc(100vh-80px)] bg-background-darkened px-[116px]">
+      <div className="flex items-center justify-between w-full pb-6 pt-11">
+        <Skeleton className="w-[400px] h-[40px] rounded-full" />
+
+        <div className="flex items-center gap-5">
+          <div className="flex flex-col">
+            <Skeleton className="w-[113px] h-[40px] rounded-full" />
+          </div>
+          <div className="flex items-center justify-center">
+            <Skeleton className="w-[22px] h-[22px]" />
+          </div>
+          <div className="flex flex-col">
+            <Skeleton className="w-[113px] h-[40px] rounded-full" />
+          </div>
+
+          <Skeleton className="w-[48px] h-[48px]" />
+
+          <div className="border-l border-solid border-[#DEDEE3] h-6 my-auto"></div>
+
+          <Skeleton className="w-[48px] h-[48px]" />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
