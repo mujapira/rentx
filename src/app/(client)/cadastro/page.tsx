e1a3386e90569871823d6c68ccd55b43c266ab55
Mujapira <malmasmello2@gmail.com>
2023-08-13 20:02:11 -0300

    fix deploy issue

---
+++ src/app/(client)/cadastro/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Cadastro() {
+  return (
+    <div className="flex flex-col items-center justify-start w-full h-full pb-4 bg-background-darkened min-h-[calc(100vh-80px)]">
+      <h1>cadastro</h1>
+    </div>
+  );
+}
